8ceee1156063222474d54050fac62d08b8d729fe
javadevelopr <java.developer.cl@gmail.com>
2019-12-23 19:47:51 -0800

    ...

--- L2/README.md
+++ L2/README.md
@@ -3,3 +3,8 @@
 ### Electric cars classifier
 A Streamlit interface to test a CNN to predict classes of popular electric cars.
 User should be able to enter an image url and get a predicted label.
+
+#### To Run:
+```
+streamlit run app.py
+```
--- L2/app.py
+++ L2/app.py
@@ -3,7 +3,7 @@
 #
 # Date Created: Dec 21,2019
 #
-# Last Modified: Sat Dec 21 21:48:43 2019
+# Last Modified: Mon Dec 23 19:41:57 2019
 #
 # Author: samolof
 #
@@ -19,15 +19,50 @@ import pandas as pd
 from PIL import Image
 import os, requests, tempfile
 
-path=Path.cwd()
-tempImgPath= path/'tmp'
 
-def download(url):
+
+def download(fileName):
+    filePath = tmpDir/fileName
+    if os.path.exists(filePath):
+        if os.path.getsize(filePath) == TRAINING_MODELS[fileName][1]:
+            return
+
+    warning, progressBar = None, None
+    try:
+        warning = st.warning("Downloading %s..." % fileName)
+        progressBar = st.progress(0)
+
+        url = TRAINING_MODELS[fileName][0]
+
+        with open(filePath, 'wb') as outfile:
+            with requests.get(url, stream=True) as r:
+                    r.raise_for_status()
+                    contentLength = len(r.content)
+                    counter = 0.0
+                    
+                    for data in r.iter_content(chunk_size=8192):
+                        if not data: break
+                        counter += len(data)
+                        outfile.write(data)
+
+                        warning.warning("Downloading %s... (%6.2f/%6.2f MB)" % (fileName, counter, contentLength))
+                        progressBar.progress(min(counter/contentLength, 1.0))
+    finally:
+        if warning is not None:
+            warning.empty()
+        if progressBar is not None:
+            progressBar.empty()
+
+
+
+
+#@st.cache
+def downloadImage(url):
     try:
         data = requests.get(url)
         imgData = data.content
         
-        tmpImgFile = tempfile.TemporaryFile(dir=tempImgPath)
+        tmpImgFile = tempfile.TemporaryFile(dir=tmpDir)
         tmpImgFile.write(imgData)
     
 
@@ -35,23 +70,72 @@ def download(url):
     except:
         return None
 
-learner = load_learner(path) 
 
-st.title('Popular Electric car models image classifier')
+IMAGE_WIDTH=400
+path=Path.cwd()
+tmpDir= path/'.tmp'
+tmpDir.mkdir(exist_ok=True)
 
-imgURL = st.sidebar.text_input('Enter image url:')
 
-st.subheader('Image')
-if imgURL != None and imgURL !='':
-    img,imgFile = download(imgURL)
-    st.image(img)
+TRAINING_MODELS = {
+        "car_vs_other.pkl" : ["https://javadevelopr865-fastai.s3-us-west-1.amazonaws.com/car_vs_other.pkl", 87390512],
+        "trained_model.pkl": ["https://javadevelopr865-fastai.s3-us-west-1.amazonaws.com/trained_model.pkl",102802036]
+}
+
+correct = wrong = 0
+
+def main():
+
+
+    st.title('Name That (Electric)Car')
+
+
+    models = list(TRAINING_MODELS.keys())
+
+    with st.spinner('Downloading pre-trained model...'):
+        for fileName in models:
+            download(fileName)
+
+
+    imgURL = st.text_input('Enter url for image:')
+
+    imgFile = st.sidebar.file_uploader('Upload image file(s):')
     
-    fImg = open_image(imgFile)
-    pred_class, pred_idx, outputs = learner.predict(fImg)
-    st.write('Predicted class:', pred_class)
+    testIfCar = load_learner(path/'.tmp', models[0]) 
+    learner = load_learner(path/'.tmp', models[1])
 
-    imgFile.close()
-else:
-    st.text('No image yet')
+    if imgURL != None and imgURL !='':
+        img,imgFile = downloadImage(imgURL)
+        st.image(img, width=IMAGE_WIDTH)
+    
+        tmpImg = open_image(imgFile)
+        pred_class, pred_idx, outputs = testIfCar.predict(tmpImg)
+        
+        if pred_class.__str__() == 'other':
+           st.info("Image doesn't appear to contain a car. Try again.")
+           return
+        else:
+            pred_class, _, _ = learner.predict(tmpImg)
+            st.write(pred_class)
 
 
+        option = st.selectbox(
+            'Is this correct?',
+            ('Yes', 'No')
+        )
+
+        global correct, wrong
+        if option == 'Yes':
+            correct += 1
+        elif option == 'No':
+            wrong += 1
+
+        #cprogress = st.progress(correct)
+        #wprogress = st.progress(wrong)
+
+        imgFile.close()
+    else:
+        st.text('No image yet')
+
+if __name__=="__main__":
+    main()
